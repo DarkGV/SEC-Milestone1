08d5bbdee233b73c8dc5b2592e3ad7a20e8de079
amsce-iscteiul <Ana_Marta_Contente@iscte-iul.pt>
2021-04-16 16:38:21 +0100

    Update App.java
    tests made on grid

--- milestone1/src/main/java/pt/ulisboa/tecnico/App.java
+++ milestone1/src/main/java/pt/ulisboa/tecnico/App.java
@@ -9,6 +9,7 @@ import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
+import java.util.ArrayList;
 
 /**
  * Hello world!
@@ -19,7 +20,7 @@ public class App
     public static void main( String[] args ) throws Exception {
         System.out.println("hello world");
 
-
+/*
         // Diffie Helman test
         DiffieHellman user = new DiffieHellman();
         DiffieHellman server = new DiffieHellman();
@@ -47,6 +48,37 @@ public class App
         System.out.println("Is Integer " + Crypto.checkIntegrity(original_mac,message,u));
 
         System.out.println("Not Integer " + Crypto.checkIntegrity(original_mac,"fake message",u));
+*/
+        //Test grid
+        ArrayList<String> users = new ArrayList<>();
+        users.add("user1");
+        users.add("user2");
+        users.add("user3");
+        users.add("user4");
+        users.add("user5");
+        ArrayList<String> by = new ArrayList<>();
+        by.add("user6");
+        by.add("user7");
+        by.add("user8");
+
+
+        Grid g = new Grid(4,4,users,by);
+        g.getGrid();
+        System.out.println("current position" + g.getUserPosition("user1"));
+        System.out.println("Nesr users " +g.getNearUsers("user1"));
+        g.getGrid();
+
+
+        g.newUser("user10",20,0);
+        System.out.println("current position" + g.getUserPosition("user10"));
+        g.updateGrid();
+        System.out.println("" +
+                "" +
+                "" +
+                "" +
+                "");
+        g.getGrid();
+
 
 
 
