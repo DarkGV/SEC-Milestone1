42fd22b3de735fa944c8d457688aac08c449a0a6
amsce-iscteiul <Ana_Marta_Contente@iscte-iul.pt>
2021-04-16 18:50:07 +0100

    Update ClientServerConnection.java
    added encryption , sends mac and time, diffie

--- milestone1/src/main/java/pt/ulisboa/tecnico/milestone1/client/ClientServerConnection.java
+++ milestone1/src/main/java/pt/ulisboa/tecnico/milestone1/client/ClientServerConnection.java
@@ -1,34 +1,107 @@
 package pt.ulisboa.tecnico.milestone1.client;
 
-import org.springframework.web.client.RestTemplate;
+import pt.ulisboa.tecnico.cryptography.Crypto;
+import pt.ulisboa.tecnico.cryptography.DiffieHellman;
+
+import javax.crypto.SecretKey;
+import java.io.File;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.HashMap;
 
 public class ClientServerConnection {
     static final String URI = "http://localhost:8080"; // Base location
+    private DiffieHellman diffie;
+    private SecretKey secretKey;
 
     static void submitLocationReport(ClientProve prove) {
         RestTemplate clientProveJSONTemplate = new RestTemplate();
         clientProveJSONTemplate.postForObject(URI + "/submitLocationReport",
                                                 prove, ClientProve.class);
     }
+     public boolean beginHandshake() throws Exception {
+        File crtFile = new File(PROJECT_PATH + PATH_TO_CERTIFICATE);
+        String certificate = Files.readString(crtFile.toPath(), Charset.defaultCharset());
+        diffie = new DiffieHellman();
+        String userPubKey = diffie.diffieUserPublicKey();
+        RestTemplate clientProveRequestLocationTemplate = new RestTemplate();
+        Map<String, String> requestParameters = new HashMap<>();
+        requestParameters.put("certificate",certificate);
+        requestParameters.put("key", userPubKey);
+        String hsrep =clientProveRequestLocationTemplate.getForEntity(URI + "/obtainLocationReport/{certificate}/{key}", String, requestParameters);
+        JsonObject repJson = JsonParser.parseString(hsrep).getAsJsonObject();
+        String serverdiffiekey = requestJson.get("key").getAsString();
+        if( serverdiffiekey ==null) {
+            String mac = requestJson.get("mac").getAsString();
 
-    static void obtainLocationReport(String userNameToSearch, String epoch, String requestOwnerName, boolean isHa) {
-       RestTemplate clientProveRequestLocationTemplate = new RestTemplate();
-       Map<String, String> requestParameters = new HashMap<>();
-       requestParameters.put("userId", userNameToSearch);
-       requestParameters.put("epoch", epoch);
-       requestParameters.put("callerUserId", requestOwnerName);
-       requestParameters.put("isHa", Boolean.toString(isHa));
-       clientProveRequestLocationTemplate.getForEntity(URI + "/obtainLocationReport/{userId}/{epoch}/{callerUserId}/{isHa}", null, requestParameters);
+
+            // receive key
+            secretkey = diffie.generateSharedSecret(serverdiffiekey);
+
+            return checkIntegrity(mac, serverdiffiekey, secretkey);
+        }else
+            return false;
+
+
+
     }
 
-    static void obtainUsersAtLocation(String pos, String epoch, boolean isHa) {
-       RestTemplate clientProveRequestLocationTemplate = new RestTemplate();
-       Map<String, String> requestParameters = new HashMap<>();
-       requestParameters.put("pos", pos);
-       requestParameters.put("epoch", epoch);
-       requestParameters.put("isHa", Boolean.toString(isHa));
-       clientProveRequestLocationTemplate.getForEntity(URI + "/obtainUsersAtLocation/{pos}/{epoch}/{isHa}", null, requestParameters);
+
+
+    void obtainLocationReport(String userNameToSearch, String epoch, String requestOwnerName, boolean isHa) throws Exception {
+        if( beginHandshake()  ) {
+            RestTemplate clientProveRequestLocationTemplate = new RestTemplate();
+            Map<String, String> requestParameters = new HashMap<>();
+            JsonObject request = new JsonObject();
+
+            JsonObject params = JsonParser.parseString("{}").getAsJsonObject();
+            params.addProperty("userId", userNameToSearch);
+            params.addProperty("epoch", epoch);
+            params.addProperty("callerUserId", requestOwnerName);
+            params.addProperty("isHa", Boolean.toString(isHa));
+            params.add("time", LocalDateTime.now().format(formatter));
+
+            String message = params.toString();
+
+            request.addProperty("mac", Crypto.getMAC(message,secretKey));
+            String[] enc = Crypto.encryptWithSecretKey(message,secretKey);
+            request.addProperty("message", enc);
+
+            requestParameters.put("data", request.toString());
+            clientProveRequestLocationTemplate.getForEntity(URI + "/obtainLocationReport/{data}", null, requestParameters);
+
+
+        }else
+            System.out.println("Connection error");
+    }
+
+    void obtainUsersAtLocation(String pos, String epoch, boolean isHa) throws Exception {
+        if( beginHandshake()  ) {
+
+            RestTemplate clientProveRequestLocationTemplate = new RestTemplate();
+            Map<String, String> requestParameters = new HashMap<>();
+            JsonObject request = new JsonObject();
+
+            JsonObject params = JsonParser.parseString("{}").getAsJsonObject();
+            params.addProperty("pos", pos);
+            params.addProperty("epoch", epoch);
+            params.addProperty("isHa", Boolean.toString(isHa));
+            params.add("time", LocalDateTime.now().format(formatter));
+
+            String message = params.toString();
+
+            request.addProperty("mac", Crypto.getMAC(message,secretKey));
+            String[] enc = Crypto.encryptWithSecretKey(message,secretKey);
+            request.addProperty("message", enc);
+
+            requestParameters.put("data", request.toString());
+
+            clientProveRequestLocationTemplate.getForEntity(URI + "/obtainUsersAtLocation/{data}", null, requestParameters);
+        }else
+            System.out.println("Connection error");
     }
 }
